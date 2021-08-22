aaeb3828129d4e82063ad108601d5cd88710ab54
rht <rhtbot@protonmail.com>
2021-08-22 01:15:19 -0400

    Add more extensions

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 mkdir -p extensions
+
 if [ ! -d extensions/Medik ]; then
     echo "Downloading Medik skin"
     wget https://bitbucket.org/wikiskripta/medik/get/master.tar.gz
@@ -9,4 +10,26 @@ if [ ! -d extensions/Medik ]; then
     mv wikiskripta-medik-* extensions/Medik
 fi
 
+# https://www.mediawiki.org/wiki/Extension:PDFEmbed
+if [ ! -d extensions/PDFEmbed ]; then
+    echo "Downloading PDFEmbed"
+    wget https://gitlab.com/hydrawiki/extensions/PDFEmbed/-/archive/2.0.2/PDFEmbed-2.0.2.zip
+    unzip PDFEmbed-2.0.2.zip
+    mv PDFEmbed-2.0.2/ extensions/PDFEmbed
+fi
+
+# https://github.com/wikimedia/mediawiki-extensions-intersection
+# We have a problem getting a permanent download url for this extension.
+#if [ ! -d extensions/intersection ]; then
+#    echo "Downloading intersection"
+#    wget 
+#fi
+
+# https://www.mediawiki.org/wiki/Extension:EmbedVideo
+if [ ! -d extensions/EmbedVideo ]; then
+    echo "Downloading EmbedVideo"
+    wget https://gitlab.com/hydrawiki/extensions/EmbedVideo/-/archive/v2.9.0/EmbedVideo-v2.9.0.zip
+    unzip EmbedVideo-v2.9.0.zip
+    mv EmbedVideo-v2.9.0 extensions/EmbedVideo
+fi
 docker build -t aqua-1.35 .
