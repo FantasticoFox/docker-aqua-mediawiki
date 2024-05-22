2351d817ca005ec70635bfdab6985f2edf6be634
itstimetoforget <zusel7.zusel7@gmail.com>
2024-05-22 21:14:18 +0200

    install wget

--- actionfiles/build_and_push_docker_image/setup/install_plugins.sh
+++ actionfiles/build_and_push_docker_image/setup/install_plugins.sh
@@ -1,5 +1,7 @@
 #versions
 pdfembed_version=2.0.2
+openIdConnect_version=OpenIDConnect-REL1_41-dc7b06b.tar.gz
+pageForms_version=PageForms-REL1_41-6415072.tar.gz
 
 # https://www.mediawiki.org/wiki/Extension:PDFEmbed
 if [ ! -d extensions/PDFEmbed ]; then
@@ -7,4 +9,16 @@ if [ ! -d extensions/PDFEmbed ]; then
     wget https://gitlab.com/hydrawiki/extensions/PDFEmbed/-/archive/${pdfembed_version}/PDFEmbed-${pdfembed_version}.zip
     unzip PDFEmbed-${pdfembed_version}.zip
     mv PDFEmbed-${pdfembed_version}/ extensions/PDFEmbed
+fi
+
+if [ ! -d extensions/OpenIDConnect ]; then
+    echo "Downloading OpenIDConnect"
+    wget https://extdist.wmflabs.org/dist/extensions/${openIdConnect_version}
+    tar -xf ${openIdConnect_version} -C extensions/
+fi
+
+if [ ! -d extensions/PageForms ]; then
+    echo "Downloading PageForms"
+    wget https://extdist.wmflabs.org/dist/extensions/${pageForms_version}
+    tar -xf ${pageForms_version} -C extensions/
 fi
